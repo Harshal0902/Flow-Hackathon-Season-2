79457ef23422500818505cff96b04cd8bcdb7b7b
Harshal Raikwar <harshalraikwar07@gmail.com>
2023-07-11 21:32:50 +0530

    🚚 Migrate from React to Next Js

---
+++ client/src/app/layout.js
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Montserrat } from 'next/font/google'
+import Navbar from '../components/Navbar'
+
+const inter = Montserrat({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'APP NAME',
+  description: 'An gaming asset NFT marketplace.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1B2431",
+        secondary: "#016795",
+        tertiary: "#1E488F",
+      },
+      zIndex: {
+        "-1": "-1",
+      },
+      spacing: {
+        '128': '40rem',
+        '256': '80rem',
+      },
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat']
+    }
+  },
+  variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+  },
+  plugins: [],
+}
